e8ae1822f1005a5079bd8c2f3f23015fa16ff0dc
karim fanti <karimwebdevelopment@gmail.com>
2021-05-06 16:34:15 +0200

    imported main files

--- src/script/app.js
+++ src/script/app.js
@@ -1,10 +1,2 @@
-const toggler = document.querySelector('.header__mobile-toggler')
-
-toggler.addEventListener('click', showMenu)
-
-function showMenu() {
-  toggler.classList.toggle('active')
-  document.querySelector('.header__navigation').classList.toggle('active')
-  document.querySelector('.overlay').classList.toggle('active')
-  document.querySelector('html').classList.toggle('active')
-}
+import showMenu from './mobileMenu'
+import showCard from './shortUrl'
